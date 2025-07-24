0deee15cc2bfc6a34e769af248e9bf0e83ab95a6
Hance08 <hancechin08@gmail.com>
2025-07-24 16:59:16 +0800

    新增多張圖片及事件，更新時間軸內容以反映2019至2024年的重要回憶，並調整部分描述以增強情感表達。

--- src/data/timelineData.js
+++ src/data/timelineData.js
@@ -1,3 +1,4 @@
+import img_2019_09_22 from '../assets/images/2019-09-22.jpeg';
 import img_2019_09_24 from '../assets/images/2019-09-24.jpeg';
 import img_2019_10_30 from '../assets/images/2019-10-30.jpeg';
 import img_2019_11_11 from '../assets/images/2019-11-11.jpeg';
@@ -22,12 +23,15 @@ import img_2021_12_25 from '../assets/images/2021-12-25.jpeg';
 import img_2022_05_20 from '../assets/images/2022-05-20.jpeg';
 import img_2022_06_02 from '../assets/images/2022-06-02.jpeg';
 import img_2022_08_11 from '../assets/images/2022-08-11.jpeg';
+import img_2022_09_30 from '../assets/images/2022-09-30.jpeg';
 import img_2023_01_01 from '../assets/images/2023-01-01.jpeg';
 import img_2023_08_11 from '../assets/images/2023-08-11.jpeg';
+import img_2023_09_30 from '../assets/images/2023-09-30.jpeg';
 import img_2023_11_26 from '../assets/images/2023-11-26.jpeg';
 import img_2024_03_29_1 from '../assets/images/2024-03-29-1.jpeg';
 import img_2024_03_29_2 from '../assets/images/2024-03-29-2.jpeg';
 import img_2024_08_11_1 from '../assets/images/2024-08-11-1.jpeg';
+import img_2024_09_30 from '../assets/images/2024-09-30.jpeg';
 import img_2024_07_01 from '../assets/images/2024-07-01.jpeg';
 import img_2025_01_12 from '../assets/images/2025-01-12.jpeg';
 import img_2025_06_20 from '../assets/images/2025-06-20.jpeg';
@@ -38,9 +42,17 @@ export const timelineData = [
     title: "愛情的開始",
     events: [
       {
+        date: "2019-09-22",
+        title: "第一次約會",
+        description: "第一次約會，我們吃吉野家，我竟然沒帶夠錢，只好兩人吃一碗，第一次約會就搞笑～～",
+        isSpecial: false,
+        hasPhoto: true,
+        image: img_2019_09_22
+      },
+      {
         date: "2019-09-24",
-        title: "第一次相遇",
-        description: "命運讓我們相遇",
+        title: "第一次麥當勞",
+        description: "是妳陪我吃麥當勞～",
         isSpecial: false,
         hasPhoto: true,
         image: img_2019_09_24
@@ -48,7 +60,7 @@ export const timelineData = [
       {
         date: "2019-09-30",
         title: "在一起的日子",
-        description: "正式開始我們的愛情故事 💕",
+        description: "這天是個颱風天\n然而就在這個颱風假\n我們在一起了 ",
         isSpecial: true,
         hasPhoto: false,
         specialType: "anniversary",
@@ -64,7 +76,7 @@ export const timelineData = [
       {
         date: "2019-11-11",
         title: "第一個光棍節",
-        description: "不再是單身的光棍節",
+        description: "連光棍節也要黑皮",
         isSpecial: false,
         hasPhoto: true,
         image: img_2019_11_11
@@ -72,7 +84,7 @@ export const timelineData = [
       {
         date: "2019-11-14",
         title: "第一次情人節",
-        description: "第一個屬於我們的情人節",
+        description: "第一個情人節送妳一盒金莎",
         isSpecial: false,
         hasPhoto: true,
         image: img_2019_11_14
@@ -80,7 +92,7 @@ export const timelineData = [
       {
         date: "2019-11-17",
         title: "特別的一天",
-        description: "那個美好的日子",
+        description: "每天都要打電話視訊\n用奇怪的特效逗對方笑",
         isSpecial: false,
         hasPhoto: true,
         image: img_2019_11_17
@@ -88,39 +100,39 @@ export const timelineData = [
       {
         date: "2019-11-20",
         title: "運動會",
-        description: "一起參加的運動會",
+        description: "一起參加的運動會\n雖然我們好像都得A流",
         isSpecial: false,
         hasPhoto: true,
         image: img_2019_11_20
       },
       {
         date: "2019-11-25",
-        title: "美好時光",
-        description: "又一個值得紀念的日子",
+        title: "一起放學",
+        description: "每天都會一起手牽手放學回家",
         isSpecial: false,
         hasPhoto: true,
         image: img_2019_11_25
       },
       {
         date: "2019-11-30",
-        title: "兩個月了",
-        description: "兩個月的甜蜜時光",
+        title: "兩個月了喔～",
+        description: "一起做鬼臉～～ 👻👻",
         isSpecial: false,
         hasPhoto: true,
         image: img_2019_11_30
       },
       {
         date: "2019-12-01",
-        title: "12月的開始",
-        description: "新的一個月，新的開始",
+        title: "真的要嫁喔！",
+        description: "妳說的喔！\n 快到了喔",
         isSpecial: false,
         hasPhoto: true,
         image: img_2019_12_01
       },
       {
         date: "2019-12-04",
-        title: "溫馨時光",
-        description: "平凡但溫暖的日子",
+        title: "耶誕城",
+        description: "第一次去耶誕城\n 等妳練完旗\n 我們搭自強號去的",
         isSpecial: false,
         hasPhoto: true,
         image: img_2019_12_04
@@ -132,17 +144,9 @@ export const timelineData = [
     title: "甜蜜成長",
     events: [
       {
-        date: "2020-02-03",
-        title: "新年新氣象",
-        description: "2020年的美好開始",
-        isSpecial: false,
-        hasPhoto: true,
-        image: null
-      },
-      {
         date: "2020-02-10",
-        title: "特別的二月",
-        description: "冬日裡的溫暖",
+        title: "太喜歡了",
+        description: "很爽喔～ 😏😏😏",
         isSpecial: false,
         hasPhoto: true,
         image: img_2020_02_10
@@ -150,15 +154,15 @@ export const timelineData = [
       {
         date: "2020-02-21",
         title: "一起重補考",
-        description: "一起努力，一起成長",
+        description: "一起回學校重補考\n 比三是要補考三科的意思嗎\n 然後我就被當了",
         isSpecial: false,
         hasPhoto: true,
         image: img_2020_02_21
       },
       {
         date: "2020-03-10",
-        title: "春天的約會",
-        description: "春暖花開的美好時光",
+        title: "戒指",
+        description: "還記得我們在吃早到晚到\n 都會把吸管套做成戒指\n 戴在手上",
         isSpecial: false,
         hasPhoto: true,
         image: img_2020_03_10
@@ -166,15 +170,15 @@ export const timelineData = [
       {
         date: "2020-03-30",
         title: "半年紀念送我禮物",
-        description: "半年的愛情結晶 🎁",
+        description: "實在是太用心了\n 好感動",
         isSpecial: false,
         hasPhoto: true,
         image: img_2020_03_30
       },
       {
         date: "2020-04-24",
-        title: "第一次幫你綁頭髮",
-        description: "溫柔的小確幸",
+        title: "第一次幫妳綁頭髮",
+        description: "我綁的還行吧～～",
         isSpecial: false,
         hasPhoto: true,
         image: img_2020_04_24
@@ -182,22 +186,22 @@ export const timelineData = [
       {
         date: "2020-07-22",
         title: "第一次去動物園",
-        description: "和動物們一起的快樂時光 🐾",
+        description: "第一次去動物園\n真的很認真在看動物的我們",
         isSpecial: false,
         hasPhoto: true,
         image: img_2020_07_22
       },
       {
         date: "2020-08-11",
-        title: "第一次過你的生日",
-        description: "為妳慶祝的第一個生日 🎂",
+        title: "第一次過妳的生日",
+        description: "因為剛好卡到籃球營\n我還特地冒著大雨回來幫妳過生日\n還送妳一個超大的看板卡片 ❤️‍🔥",
         isSpecial: false,
         hasPhoto: false
       },
       {
         date: "2020-08-24",
-        title: "第一次去看你表演",
-        description: "台上發光的妳最美 ✨",
+        title: "第一次去看妳表演",
+        description: "到新勢公園和中正公園看妳表演\n頂著一顆爆炸頭找妳",
         isSpecial: false,
         hasPhoto: true,
         image: img_2020_08_24
@@ -220,7 +224,7 @@ export const timelineData = [
       {
         date: "2021-04-24",
         title: "一起去畢旅",
-        description: "畢業旅行的美好回憶 🎒",
+        description: "一起去高中畢旅～\n還記得妳跟我借了一雙破洞的襪子",
         isSpecial: false,
         hasPhoto: true,
         image: img_2021_04_24
@@ -228,7 +232,7 @@ export const timelineData = [
       {
         date: "2021-08-11",
         title: "17歲了",
-        description: "妳的17歲生日 🎉",
+        description: "妳的17歲生日 🎉\n 我們吃了金大鋤 (不好吃\n 再也不去",
         isSpecial: false,
         hasPhoto: true,
         image: img_2021_08_11
@@ -236,7 +240,7 @@ export const timelineData = [
       {
         date: "2021-09-30",
         title: "兩週年了",
-        description: "兩年的愛情長跑 💖",
+        description: "兩年囉～～～好快",
         isSpecial: true,
         hasPhoto: false,
         specialType: "anniversary",
@@ -244,8 +248,8 @@ export const timelineData = [
       },
       {
         date: "2021-11-06",
-        title: "最後一次運動會了",
-        description: "學生時代的最後一次運動會",
+        title: "最後一次運動會",
+        description: "高中的最後一次運動會\n 我們拍了一張可愛的照片",
         isSpecial: false,
         hasPhoto: true,
         image: img_2021_11_06
@@ -253,7 +257,7 @@ export const timelineData = [
       {
         date: "2021-12-25",
         title: "聖誕節",
-        description: "溫馨的聖誕節 🎄",
+        description: "Marry Christmas 🎄",
         isSpecial: false,
         hasPhoto: true,
         image: img_2021_12_25
@@ -267,15 +271,15 @@ export const timelineData = [
       {
         date: "2022-05-20",
         title: "520快樂",
-        description: "我愛妳的日子 ❤️",
+        description: "我愛妳的日子 ❤️\n終於考完統測\n可以蒿蒿爽爽了",
         isSpecial: false,
         hasPhoto: true,
         image: img_2022_05_20
       },
       {
         date: "2022-06-02",
-        title: "要畢業了",
-        description: "學生時代的結束，新階段的開始 🎓",
+        title: "高中畢業",
+        description: "高中畢業了🎓\n以後要回來看老師和學生\n北漂大學要開始了 ",
         isSpecial: false,
         hasPhoto: true,
         image: img_2022_06_02
@@ -283,7 +287,7 @@ export const timelineData = [
       {
         date: "2022-08-11",
         title: "18歲了",
-        description: "成年的妳更加美麗 ✨",
+        description: "小妹妹要變大姊姊了 ✨",
         isSpecial: false,
         hasPhoto: true,
         image: img_2022_08_11
@@ -291,11 +295,11 @@ export const timelineData = [
       {
         date: "2022-09-30",
         title: "三週年了",
-        description: "三年的深情厚愛 💕",
+        description: "殊不知就三年\n大餐肯定是我們都愛吃的壽喜燒",
         isSpecial: true,
-        hasPhoto: false,
+        hasPhoto: true,
         specialType: "anniversary",
-        image: null
+        image: img_2022_09_30
       }
     ]
   },
@@ -306,7 +310,7 @@ export const timelineData = [
       {
         date: "2023-01-01",
         title: "第一次一起跨年",
-        description: "新年的第一個擁抱 🎊",
+        description: "第一次去101現場看煙火",
         isSpecial: false,
         hasPhoto: true,
         image: img_2023_01_01 
@@ -314,7 +318,7 @@ export const timelineData = [
       {
         date: "2023-08-11",
         title: "19歲了",
-        description: "19歲的美麗妳 🌟",
+        description: "19歲啦",
         isSpecial: false,
         hasPhoto: true,
         image: img_2023_08_11
@@ -322,16 +326,16 @@ export const timelineData = [
       {
         date: "2023-09-30",
         title: "四週年了",
-        description: "四年的相伴相守 💎",
+        description: "四年的相伴相守\n我們跑去文化大學情人坡\n第一次一起看夜景",
         isSpecial: true,
-        hasPhoto: false,
+        hasPhoto: true,
         specialType: "anniversary",
-        image: null
+        image: img_2023_09_30
       },
       {
         date: "2023-11-26",
-        title: "第一次跟你同學一起出去玩",
-        description: "和朋友們的快樂時光 🎉",
+        title: "第一次跟妳同學出去玩",
+        description: "青青草原～",
         isSpecial: false,
         hasPhoto: true,
         image: img_2023_11_26
@@ -345,7 +349,7 @@ export const timelineData = [
       {
         date: "2024-03-29",
         title: "第一次一起去平溪放天燈",
-        description: "許願的浪漫時刻 🏮",
+        description: "第一次一起放天燈 🏮\n希望願望都會實現",
         isSpecial: false,
         hasPhoto: true,
         image: img_2024_03_29_1
@@ -353,7 +357,7 @@ export const timelineData = [
       {
         date: "2024-07-01",
         title: "第一次一起去九份",
-        description: "山城的美麗回憶 🏔️",
+        description: "去九份挖金礦\n可惜沒挖到",
         isSpecial: false,
         hasPhoto: true,
         image: img_2024_07_01
@@ -361,7 +365,7 @@ export const timelineData = [
       {
         date: "2024-08-11",
         title: "20歲了",
-        description: "20歲的妳，最美的年華 💐",
+        description: "20歲了\n送了妳一條潘朵拉項鏈\n一直戴戴到現在",
         isSpecial: false,
         hasPhoto: true,
         image: img_2024_08_11_1
@@ -369,11 +373,11 @@ export const timelineData = [
       {
         date: "2024-09-30",
         title: "五週年了",
-        description: "五年的愛情馬拉松 🏆",
+        description: "吃好吃的瓦城",
         isSpecial: true,
-        hasPhoto: false,
+        hasPhoto: true,
         specialType: "anniversary",
-        image: null
+        image: img_2024_09_30
       }
     ]
   },
@@ -384,19 +388,19 @@ export const timelineData = [
       {
         date: "2025-01-12",
         title: "第一次一起去日本",
-        description: "期待的日本之旅 🇯🇵",
+        description: "第一次跟妳一起出國\n日本之旅 🇯🇵",
         isSpecial: false,
         hasPhoto: true,
-        isFuture: true,
+        isFuture: false,
         image: img_2025_01_12
       },
       {
         date: "2025-06-20",
         title: "又快要畢業了",
-        description: "另一個新的開始 🎓",
+        description: "哎狗～～～\n怎麼有美女",
         isSpecial: false,
         hasPhoto: true,
-        isFuture: true,
+        isFuture: false,
         image: img_2025_06_20
       } 
     ]
